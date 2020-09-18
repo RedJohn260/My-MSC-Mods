ee8898bd7bc81affc55854474cae9c020e56b397
RedJohn260 <damir.k93@gmail.com>
2020-09-18 07:01:50 +0200

    GT_Interior v1.1 update

--- GT_InteriorCustom/GT_Interior/GT_Interior.cs
+++ GT_InteriorCustom/GT_Interior/GT_Interior.cs
@@ -1,6 +1,8 @@
 ﻿using MSCLoader;
 using UnityEngine;
 using HutongGames.PlayMaker;
+using System;
+using System.IO;
 
 namespace GT_Interior
 {
@@ -19,13 +21,11 @@ namespace GT_Interior
         private Material Panels;
         private Material Seats;
         private GameObject meters;
-        private GameObject console;
         private GameObject column;
-        private GameObject gwheel;
-        private GameObject gwheel1;
-        private GameObject stick;
         private GameObject ladica;
         private Material gtdash;
+        private float specular_range = 1;
+        private float specular_smoothness = 1;
         public string message = "<b><color=green>Original Mod Link:</color></b> https://www.nexusmods.com/mysummercar/mods/91 <color=orange>Any re-upload of this mod is strictly prohibited. I do not support any of my mods uploaded to different sites without my permission.</color> <b><color=green>Sincerely RedJohn260.</color></b>";
         public string tittle = "Satsuma GT Interior";
         public override void OnLoad()
@@ -36,14 +36,76 @@ namespace GT_Interior
             Seats = ab.LoadAsset("Seats.mat") as Material;
             gtdash = ab.LoadAsset("gtdash.mat") as Material;
 
-            column = Object.Instantiate(ab.LoadAsset<GameObject>("gtcolumn.prefab"));
-            console = Object.Instantiate(ab.LoadAsset<GameObject>("gtconsole.prefab"));
-            meters = Object.Instantiate(ab.LoadAsset<GameObject>("gtmeters.prefab"));
-            gwheel = Object.Instantiate(ab.LoadAsset<GameObject>("gtwheel.prefab"));
-            gwheel1 = Object.Instantiate(ab.LoadAsset<GameObject>("gtwheel1.prefab"));
-            stick = Object.Instantiate(ab.LoadAsset<GameObject>("gtstick.prefab"));
-            ladica = Object.Instantiate(ab.LoadAsset<GameObject>("ladica.prefab"));
+            GameObject gameObject1 = ab.LoadAsset("gtcolumn.prefab") as GameObject;
+            GameObject gameObject2 = ab.LoadAsset("gtmeters.prefab") as GameObject;
+            GameObject gameObject3 = ab.LoadAsset("ladica.prefab") as GameObject;
+
+            column = UnityEngine.Object.Instantiate(gameObject1);
+            meters = UnityEngine.Object.Instantiate(gameObject2);
+            ladica = UnityEngine.Object.Instantiate(gameObject3);
+
+            UnityEngine.Object.Destroy(gameObject1);
+            UnityEngine.Object.Destroy(gameObject2);
+            UnityEngine.Object.Destroy(gameObject3);
+
             ab.Unload(false);
+
+            //Check for steam
+            bool SteamActive = ModLoader.CheckSteam();
+            if (SteamActive)
+            {
+                Debug.Log(" " + "Steam client found!");
+            }
+            else
+            {
+                Debug.LogError(" " + "Steam client not found!");
+            }
+
+            //Get Materials from prefabs
+            Material columnmat = column.GetComponent<MeshRenderer>().material;
+            Material metersmat = meters.GetComponent<MeshRenderer>().material;
+            Material ladicamat = ladica.GetComponent<MeshRenderer>().material;
+
+            //Assign Custom Texture To Materials
+            try
+            {
+                //diffuse
+                BodyM.mainTexture = LoadAssets.LoadTexture(this, "textures/body_roof.png");
+                Panels.mainTexture = LoadAssets.LoadTexture(this, "textures/door_panels_floor.png");
+                Seats.mainTexture = LoadAssets.LoadTexture(this, "textures/seats.png");
+                gtdash.mainTexture = LoadAssets.LoadTexture(this, "textures/dashboard.png");
+                columnmat.mainTexture = LoadAssets.LoadTexture(this, "textures/column.png");
+                metersmat.mainTexture = LoadAssets.LoadTexture(this, "textures/dashboard_meters.png");
+                ladicamat.mainTexture = LoadAssets.LoadTexture(this, "textures/glovebox.png");
+
+                //specular
+                BodyM.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/body_roof_spec.png"));
+                Panels.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/door_panels_floor_spec.png"));
+                Seats.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/seats_spec.png"));
+                gtdash.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/dashboard_spec.png"));
+                columnmat.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/column_spec.png"));
+                metersmat.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/dashboard_meters_spec.png"));
+                ladicamat.SetTexture("_MetallicGlossMap", LoadAssets.LoadTexture(this, "textures/glovebox_spec.png"));
+
+                //normal
+                BodyM.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/body_roof_norm.png"));
+                Panels.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/door_panels_floor_norm.png"));
+                Seats.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/seats_norm.png"));
+                gtdash.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/dashboard_norm.png"));
+                columnmat.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/column_norm.png"));
+                metersmat.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/dashboard_meters_norm.png"));
+                ladicamat.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "textures/glovebox_norm.png"));
+            }
+            catch (FileNotFoundException e)
+            {
+                Debug.LogException(e);
+                ModConsole.Error(" " + e.ToString());
+            }
+            
+            //Set Materials to prefabs
+            column.GetComponent<MeshRenderer>().material = columnmat;
+            meters.GetComponent<MeshRenderer>().material = metersmat;
+            ladica.GetComponent<MeshRenderer>().material = ladicamat;
 
             //panel doorf left
             GameObject.Find("door left(Clone)/panel_door_left1").GetComponent<MeshRenderer>().material = Panels;
@@ -106,26 +168,6 @@ namespace GT_Interior
                 column.transform.localPosition = new Vector3(0f, 0f, 0f);
                 column.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
             }
-            var whh = GameObject.Find("gt steering wheel(Clone)");
-            whh.GetComponent<MeshRenderer>().enabled = false;
-            gwheel.transform.SetParent(whh.transform, false);
-            gwheel.transform.localPosition = new Vector3(0f, 0f, 0f);
-            gwheel.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
-            gwheel1.transform.SetParent(whh.transform, false);
-            gwheel1.transform.localPosition = new Vector3(0f, 0f, 0f);
-            gwheel1.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
-
-            var con = GameObject.Find("SATSUMA(557kg, 248)/Dashboard").transform.FindChild("center console gt(xxxxx)");
-            con.GetComponent<MeshRenderer>().enabled = false;
-            console.transform.SetParent(con.transform, false);
-            console.transform.localPosition = new Vector3(0f, 0f, 0f);
-            console.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
-
-            var ss = GameObject.Find("SATSUMA(557kg, 248)/Dashboard").transform.FindChild("center console gt(xxxxx)/GearLever/Pivot/Lever/gear_stick");
-            ss.GetComponent<MeshRenderer>().enabled = false;
-            stick.transform.SetParent(ss.transform, false);
-            stick.transform.localPosition = new Vector3(0f, 0f, 0f);
-            stick.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
 
             var lad = GameObject.Find("dashboard(Clone)/glovbox");
             lad.GetComponent<MeshRenderer>().enabled = false;
