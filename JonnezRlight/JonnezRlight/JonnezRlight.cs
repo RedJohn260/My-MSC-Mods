2efb6438cbe5ebd46043cc748a94e7aa423d1c9b
RedJohn260 <damir.k93@gmail.com>
2020-10-01 21:37:29 +0200

    JonnezRearLight Update V1.2

--- JonnezRlight/JonnezRlight/JonnezRlight.cs
+++ JonnezRlight/JonnezRlight/JonnezRlight.cs
@@ -9,127 +9,109 @@ namespace JonnezRlight
         public override string ID => "JonnezRlight"; //Your mod ID (unique)
         public override string Name => "JonnezRlight"; //You mod name
         public override string Author => "RedJohn260"; //Your Username
-        public override string Version => "1.0.0"; //Version
+        public override string Version => "1.2"; //Version
 
-        // Set this to true if you will be load custom assets from Assets folder.
-        // This will create subfolder in Assets folder for your mod.
         public override bool UseAssetsFolder => true;
 
 
         private AssetBundle ab;
-        private GameObject lightOff;
-        private GameObject lightOn;
-        private GameObject r_spotlight;
-        private GameObject lightBrake;
+        private GameObject JonnezRearLight;
+        private GameObject LightOff;
+        private GameObject LightOn;
+        private GameObject LightBrakeOn;
+        private Light SpotLight;
         private GameObject JONNEZ;
-        private GameObject JONNEZ1;
-        private Light spotlight;
-        private Drivetrain drivetrain;
-        private AxisCarController AxisCarController;
-        private float intensity_normal = 2.0f;
-        private float intensity_high = 5.0f;
-        private GameObject lights;
-        private PlayMakerFSM PlayMakerFSM;
-        private FsmBool lightsON;
-        private GameObject plate;
-        private GameObject nosac;
-        private GameObject svijetlo_nosac;
+        private PlayMakerFSM LightPlayMaker;
+        private PlayMakerFSM RpmPlayMaker;
+        private FsmBool IsLightOn;
+        private FsmBool IsEngineOn;
+        private FsmFloat CurrentRpm;
+        private AxisCarController JonnezAxisController;
 
-
-        public override void OnNewGame()
-        {
-            // Called once, when starting a New Game, you can reset your saves here
-        }
 
         public override void OnLoad()
         {
             ab = LoadAssets.LoadBundle(this, "rlight.unity3d");
-            lightOff = GameObject.Instantiate( ab.LoadAsset("light_off.prefab")) as GameObject;
-            lightOn = GameObject.Instantiate( ab.LoadAsset("light_on.prefab")) as GameObject;
-            lightBrake = GameObject.Instantiate( ab.LoadAsset("light_brake.prefab")) as GameObject;
-            r_spotlight = GameObject.Instantiate( ab.LoadAsset("r_spotlight.prefab")) as GameObject;
-            plate = GameObject.Instantiate(ab.LoadAsset("plate.prefab")) as GameObject;
-            nosac = GameObject.Instantiate(ab.LoadAsset("nosac.prefab")) as GameObject;
-            svijetlo_nosac = GameObject.Instantiate(ab.LoadAsset("svijetlo_nosac.prefab")) as GameObject;
+            GameObject gameObject = ab.LoadAsset("JonnezRLight.prefab") as GameObject;
+            JonnezRearLight = Object.Instantiate(gameObject);
+            Object.Destroy(gameObject);
+            JONNEZ = GameObject.Find("JONNEZ ES(Clone)");
+            JonnezRearLight.transform.SetParent(JONNEZ.transform, false);
+            JonnezRearLight.transform.localPosition = new Vector3(0f, 0.241f, -0.582f);
+            LightOff = JonnezRearLight.transform.FindChild("light_off").gameObject;
+            LightOn = JonnezRearLight.transform.FindChild("light_on").gameObject;
+            LightOn.SetActive(false);
+            LightBrakeOn = JonnezRearLight.transform.FindChild("light_brake").gameObject;
+            LightBrakeOn.SetActive(false);
+            SpotLight = JonnezRearLight.transform.FindChild("light_spot").gameObject.GetComponent<Light>();
+            SpotLight.enabled = false;
             ab.Unload(false);
-
-
-            JONNEZ = GameObject.Find("JONNEZ ES(Clone)/MESH/frame");
-
-            lightOff.transform.SetParent(JONNEZ.transform, false);
-            lightOn.transform.SetParent(JONNEZ.transform, false);
-            lightBrake.transform.SetParent(JONNEZ.transform, false);
-            r_spotlight.transform.SetParent(JONNEZ.transform, false);
-            plate.transform.SetParent(JONNEZ.transform, false);
-            nosac.transform.SetParent(JONNEZ.transform, false);
-            svijetlo_nosac.transform.SetParent(JONNEZ.transform, false);
-            lightOn.SetActive(false);
-            lightBrake.SetActive(false);
-
-
-            spotlight = r_spotlight.GetComponent<Light>();
-            spotlight.intensity = intensity_normal;
-            r_spotlight.SetActive(false);
-
-            lights = GameObject.Find("JONNEZ ES(Clone)").transform.Find("LOD/Dashboard/Lights").gameObject;
-            PlayMakerFSM = lights.GetComponent<PlayMakerFSM>();
-            lightsON = PlayMakerFSM.FsmVariables.FindFsmBool("LightsOn");
-
-            JONNEZ1 = GameObject.Find("JONNEZ ES(Clone)");
-            drivetrain = JONNEZ1.GetComponent<Drivetrain>();
-            AxisCarController = drivetrain.GetComponent<AxisCarController>();
-
+            LightPlayMaker = JONNEZ.transform.FindChild("LOD/Dashboard/Lights").GetComponent<PlayMakerFSM>();
+            RpmPlayMaker = JONNEZ.transform.FindChild("Starter").GetComponent<PlayMakerFSM>();
+            JonnezAxisController = JONNEZ.GetComponent<Drivetrain>().GetComponent<AxisCarController>();
+            MeshRenderer meshRenderer = JonnezRearLight.transform.FindChild("plate").GetComponent<MeshRenderer>();
+            meshRenderer.material.SetTexture("_MainTex", LoadAssets.LoadTexture(this, "plate_diff.jpg"));
+            meshRenderer.material.SetTexture("_BumpMap", LoadAssets.LoadTexture(this, "plate_norm.jpg"));
+            meshRenderer.material.EnableKeyword("_NORMALMAP");
         }
 
-        public void LightCheck()
+        private void DayLightOn()
         {
-            if (lightsON.Value == true)
+            IsLightOn = LightPlayMaker.FsmVariables.FindFsmBool("LightsOn").Value;
+            CurrentRpm = RpmPlayMaker.FsmVariables.FindFsmFloat("RPM").Value;
+            IsEngineOn = RpmPlayMaker.FsmVariables.FindFsmBool("Starting").Value;
+            if (IsLightOn.Value == true && IsEngineOn.Value == true)
             {
-                lightOn.SetActive(true);
-                r_spotlight.SetActive(true);
+                LightOff.SetActive(false);
+                LightOn.SetActive(true);
+                SpotLight.enabled = true;
+               if (CurrentRpm.Value > 3000.603)
+               {
+                    SpotLight.intensity = CurrentRpm.Value / 1000;
+               }
             }
             else
             {
-                lightOn.SetActive(false);
-                r_spotlight.SetActive(false);
+                LightOff.SetActive(true);
+                LightOn.SetActive(false);
+                SpotLight.enabled = false;
             }
         }
-        public void BrakeCheck()
+
+        private void BrakeLightOn()
         {
-            if (AxisCarController.brakeKey == true) 
-            { 
-                lightBrake.SetActive(true);
-                spotlight.intensity = intensity_high;
-                r_spotlight.SetActive(true);
+            if (JonnezAxisController.brakeKey == true)
+            {
+                LightBrakeOn.SetActive(true);
+                LightOff.SetActive(false);
+                LightOn.SetActive(false);
+                if (IsLightOn.Value == false)
+                {
+                    SpotLight.enabled = true;
+                    SpotLight.intensity = 8.0f;
+                }
+                if (IsLightOn.Value == true)
+                {
+                    SpotLight.intensity = CurrentRpm.Value / 500;
+                }
             }
             else
             {
-                lightBrake.SetActive(false);
-                spotlight.intensity = intensity_normal;
+                LightBrakeOn.SetActive(false);
+                if (IsLightOn.Value == false)
+                {
+                    SpotLight.enabled = false;
+                    LightOff.SetActive(true);
+                    LightOn.SetActive(false);
+                    SpotLight.intensity = CurrentRpm.Value / 1000;
+                }
             }
-        }
-
-        public override void ModSettings()
-        {
-            // All settings should be created here. 
-            // DO NOT put anything else here that settings.
-        }
-
-        public override void OnSave()
-        {
-            // Called once, when save and quit
-            // Serialize your save file here.
-        }
-
-        public override void OnGUI()
-        {
-            // Draw unity OnGUI() here
         }
 
         public override void Update()
         {
-            LightCheck();
-            BrakeCheck();
+            DayLightOn();
+            BrakeLightOn();
         }
     }
 }
