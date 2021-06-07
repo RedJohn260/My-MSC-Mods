cc0ae0714deb91a229b55920f622b5358bf6153e
RedJohn260 <damir.k93@gmail.com>
2021-06-07 07:55:38 +0200

    GT Interior DD Patch

---
+++ GT_InteriorDigiDashPatch/GT_InteriorDD_Patch/GT_InteriorDD_Patch/GT_InteriorDD_Patch.cs
@@ -0,0 +1,56 @@
+﻿using MSCLoader;
+using System;
+using UnityEngine;
+
+namespace GT_InteriorDD_Patch
+{
+    public class GT_InteriorDD_Patch : Mod
+    {
+        public override string ID => "GT_InteriorDD_Patch"; //Your mod ID (unique)
+        public override string Name => "GT_InteriorDD_Patch"; //You mod name
+        public override string Author => "RedJohn260"; //Your Username
+        public override string Version => "1.0"; //Version
+
+        // Set this to true if you will be load custom assets from Assets folder.
+        // This will create subfolder in Assets folder for your mod.
+        public override bool UseAssetsFolder => false;
+        public override bool LoadInMenu => true;
+
+        private bool IsGTIntInstalled;
+        private bool IsDDInstalled;
+        public override void OnMenuLoad()
+        {
+            IsDDInstalled = ModLoader.IsModPresent("SatsumaDigidash");
+            IsGTIntInstalled = ModLoader.IsModPresent("GT_Interior");
+            if (IsGTIntInstalled && IsDDInstalled)
+            {
+                ModConsole.Print("[GT_INTERIOR_DDPATCH]: <color=green>SatsumaDigidash Mod Found!</color>");
+                ModConsole.Print("[GT_INTERIOR_DDPATCH]: <color=green>GT_Interior Mod Found!</color>");
+            }
+            else
+            {
+                ModConsole.Print("[GT_INTERIOR_DDPATCH]: <color=orange>SatsumaDigidash Mod Not Found!</color>");
+                ModConsole.Print("[GT_INTERIOR_DDPATCH]: <color=green>Continuing...</color>");
+            }
+        }
+        public override void OnLoad()
+        {
+            try
+            {
+                if (IsGTIntInstalled && IsDDInstalled)
+                {
+                    UnityEngine.Object.Destroy(GameObject.Find("gtmeters(Clone)"));
+                }
+                else
+                {
+                    ModConsole.Print("[GT_INTERIOR_DDPATCH]: <color=orange>This patch only works if GT_Interior and SatsumaDigidash Mods are installed... Otherwise it does nothing.</color>");
+                }
+            }
+            catch (Exception e)
+            {
+                ModConsole.Error(e.Message);
+                Debug.LogError(e.Message);
+            }
+        }
+    }
+}
---
+++ GT_InteriorDigiDashPatch/GT_InteriorDD_Patch/GT_InteriorDD_Patch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GT_InteriorDD_Patch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GT_InteriorDD_Patch")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71e39011-5f68-4a49-93b3-91d06b81294a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
